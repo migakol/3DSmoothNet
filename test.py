4936108332ff791be8c49beca3d6a31717843fa1
michael kolomenkin <michael.kolomenkin@gmail.com>
2020-12-26 22:25:19 +0200

    Added test file for playing with aligning of large files

---
+++ test.py
@@ -0,0 +1,10 @@
+import numpy as np
+import open3d as o3d
+
+
+if __name__ == '__main__':
+    print('Trying to align large surfaces')
+
+    filename1 = '/Users/michaelko/Downloads/wetransfer-845f7e/15dec2020_horisontalRulerSection1.ply'
+    filename2 = '/Users/michaelko/Downloads/wetransfer-845f7e/15dec2020_horisontalRulerSection2.ply'
+
